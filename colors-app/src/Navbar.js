9e9054305b0d1b5238d1338f605ce3c4f235b5c7
Michael Giebner <mikegiebner@gmail.com>
2019-11-18 10:34:06 -0500

    Add Navbar and Footer to SingleColorPalette

--- colors-app/src/Navbar.js
+++ colors-app/src/Navbar.js
@@ -34,14 +34,14 @@ class Navbar extends Component {
    }
 
    render() {
-      const { level, changeLevel } = this.props
+      const { level, changeLevel, showingAllColors } = this.props
       const { format } = this.state
       return (
          <header className="Navbar">
             <div className="logo">
                <Link to="/">React Color Picker</Link>
             </div>
-            <div className="slider-container">
+            {showingAllColors && (<div className="slider-container">
                <span>Level: {level} </span>
                <div className="slider">
                   <Slider
@@ -53,6 +53,7 @@ class Navbar extends Component {
                   />
                </div>
             </div>
+            )}
             <div className="select-container">
                <Select value={format} onChange={this.handleFormatChange}>
                   <MenuItem value="hex">HEX - #ffffff</MenuItem>
--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import ColorBox from "./ColorBox"
 import Navbar from "./Navbar"
+import PaletteFooter from "./PaletteFooter"
 import "./Palette.css"
 
 class Palette extends Component {
@@ -37,15 +38,17 @@ class Palette extends Component {
       ))
       return (
          <div>
-            <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
+            <Navbar 
+               level={level}
+               changeLevel={this.changeLevel}
+               handleChange={this.changeFormat}
+               showingAllColors
+            />
             <div className="Palette">
                <div className="Palette-colors">
                   {colorBoxes}
                </div>
-               <footer className="Palette-footer">
-                  {paletteName}
-                  <span className="emoji">{emoji}</span>
-               </footer>
+            <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
          </div>
       )
